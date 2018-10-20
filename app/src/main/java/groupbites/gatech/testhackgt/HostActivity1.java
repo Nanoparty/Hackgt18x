0989c04ad1ec9650bf16590cce7185443e51a20f
Nandar Soe <snandar.nsoe@gmail.com>
2018-10-20 16:12:05 -0400

    Got rid of HostAcitivity1 and replaced it with a blank working version

--- app/src/main/java/groupbites/gatech/testhackgt/HostActivity1.java
+++ app/src/main/java/groupbites/gatech/testhackgt/HostActivity1.java
@@ -1,31 +1,13 @@
 package groupbites.gatech.testhackgt;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Button;
 
 public class HostActivity1 extends AppCompatActivity {
-    private Button nextButton;
-    private Button backButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host1);
-        nextButton = (Button) findViewById(R.id.descriptionNextHost);
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), HostActivity2.class);
-                startActivity(intent);
-            }}
-            backButton = (Button) findViewById(R.id.descriptionBackHost);
-        backButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
     }
 }
