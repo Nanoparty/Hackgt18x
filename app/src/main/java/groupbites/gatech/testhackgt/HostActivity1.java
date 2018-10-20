b2cc7f187fb2507a73695cd73e3df39830481ced
J <jcnguyen1994@gmail.com>
2018-10-20 19:17:05 -0400

    Ambitous
    Crazy

--- app/src/main/java/groupbites/gatech/testhackgt/HostActivity1.java
+++ app/src/main/java/groupbites/gatech/testhackgt/HostActivity1.java
@@ -1,14 +1,11 @@
 package groupbites.gatech.testhackgt;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class HostActivity1 extends AppCompatActivity {
 
@@ -20,7 +17,7 @@ public class HostActivity1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host1);
         butt = (Button) findViewById(R.id.host_button);
-        dish = (TextView) findViewById(R.id.dish_name);
+        dish = (TextView) findViewById(R.id.host_event);
         butt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
