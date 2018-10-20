6e2211c002c5215096285c45f09458deffd1c88c
Kevintao100 <44214023+Kevintao100@users.noreply.github.com>
2018-10-20 14:24:09 -0400

    Host Activity

--- app/src/main/java/groupbites/gatech/testhackgt/HostActivity1.java
+++ app/src/main/java/groupbites/gatech/testhackgt/HostActivity1.java
@@ -1,7 +1,10 @@
 package groupbites.gatech.testhackgt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Button;
 
 public class HostActivity1 extends AppCompatActivity {
@@ -10,5 +13,13 @@ public class HostActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host1);
+        nextButton = (Button) findViewById(R.id.descriptionNextHost);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HostActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
