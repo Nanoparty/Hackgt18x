71db6415dfbfa1d37b7b6ed5eeece0bf8ebcf8ea
Necropuppy <nfooteaz@gmail.com>
2018-10-21 00:43:08 -0400

    kevin lol

--- app/src/main/java/groupbites/gatech/testhackgt/ApplicationActivity.java
+++ app/src/main/java/groupbites/gatech/testhackgt/ApplicationActivity.java
@@ -23,6 +23,15 @@ public class ApplicationActivity extends AppCompatActivity {
                 index = i;
         }
         search = search.substring(0,index-1);
+        Host host = null;
+        for(Host h:Host.hostList){
+            if(h.getName().toString().equals(search)){
+                
+                host = h;
+            }
+        }
+
+
 
         
 
